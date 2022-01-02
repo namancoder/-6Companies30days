0a2ed07904893fb0842ea60e78d5c9e434e5a6fb
Naman Gupta <49373509+namancoder@users.noreply.github.com>
2022-01-02 23:23:43 +0530

    day 3

---
+++ minimum_size_subarray.cpp
@@ -0,0 +1,11 @@
+  int firstPos = 0, sum = 0, minLength = INT_MAX;
+    for(int i = 0; i<nums.size(); i++) {
+        sum += nums[i];
+        while(sum >= s) {
+            minLength = min(minLength, i - firstPos + 1);
+            sum -= nums[firstPos++];
+        }
+    }
+    
+    return minLength == INT_MAX? 0 : minLength;
+  }
---
+++ squaresinagrid.cpp
@@ -0,0 +1,6 @@
+int squares(int n)
+{
+    int sum = (n*(2*n+1)*(n+1))/6;
+
+    return sum;
+}
